e5610a3dd5b871d8512aaa28af18f9a8059aa287
Flávio Henrique <digitalclasse@gmail.com>
2023-01-10 23:08:35 -0300

    first commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Home</title>
+</head>
+<h1>Olá Mundo!</h1>
+<h2>Seja bem-vindo</h2>
+
+<body>
+
+</body>
+</html>
